417e082ea2f16da6b5756e3bdde23064b69406f7
mamoru deguchi <mmr.deguchi@gmail.com>
2023-11-13 16:49:48 +0900

    テスト作成

--- test/charactor.spec.js
+++ test/charactor.spec.js
@@ -88,4 +88,34 @@ describe("Disney API Server", () => {
       });
     });
   });
+
+  describe("create", () => {
+    describe("Post /api/charactors", () => {
+      it("return status 200", async () => {
+        // リクエスト情報
+        const body = [
+          {
+            id: 99999,
+            films: ["Hercules (film)"],
+            shortFilms: [],
+            tvShows: ["Hercules (TV series)"],
+            videoGames: ["Kingdom Hearts III"],
+            parkAttractions: [],
+            allies: [],
+            enemies: [],
+            sourceUrl: "https://disney.fandom.com/wiki/Achilles_(Hercules)",
+            name: "Achilles",
+            imageUrl:
+              "https://static.wikia.nocookie.net/disney/images/d/d3/Vlcsnap-2015-05-06-23h04m15s601.png",
+          },
+        ];
+
+        // APIを呼び出す
+        const res = await request.post("/api/charactors").send(body);
+
+        // 結果の確認
+        res.should.have.status(200);
+      });
+    });
+  });
 });
